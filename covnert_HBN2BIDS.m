c635ae1ec6cf52dbe05a4ad4099cdf65ddb4291b
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-01-20 21:34:55 -0800

    minor mods on the BIDS converter

--- covnert_HBN2BIDS.m
+++ covnert_HBN2BIDS.m
@@ -11,7 +11,6 @@ clearvars
 p2l = init_paths("linux", "expanse", "HBN", 1, 1);
 f2l.elocs = p2l.eegRepo + "GSN_HydroCel_129.sfp";  % f2l = file to load
 
-task_list = ["Video-DM", "Video-FF", "Video-WK", "Video-TP", "RestingState"];
 plist = readtable("./funcs/tsv/participants_augmented.tsv", "FileType","text");
 plist.Full_Pheno = string(plist{:,"Full_Pheno"});
 plist.Commercial_Use = string(plist{:,"Commercial_Use"});
@@ -29,7 +28,6 @@ clear EEG
 %% Define tasks
 % Let's also define the tasks and potentially the release that we want to include 
 % in the BIDS
-
 target_tasks = ["Video_DM", "Video_FF", "Video_TP", "Video_WK", "RestingState"];
 target_release = ["R3"]; %#ok<NBRAK2> 
 incl_fullset_only = true; % only subjects with all datasets avalaibe will be on the BIDS
@@ -38,7 +36,6 @@ incl_fullset_only = true; % only subjects with all datasets avalaibe will be on 
 % While the files are not remedied and are not on EEGLAB's format, it is good 
 % to have the strucutre to later on use it for adjusting the raw files and then 
 % making the structure.
-
 data = struct;
 pinfo = plist.Properties.VariableNames(4:8);
 target_table = plist(string(table2array(plist(:,"release_number")))==target_release,:);
@@ -47,7 +44,6 @@ for r = 1: height(target_table)
     if incl_fullset_only
         if sum(t{1, target_tasks}) == length(target_tasks)
             data(end+1).subject = char(t.participant_id);
-            raw_path = datarepo + string(t.participant_id) + dpath;
             remedied_path = remediedrepo + string(t.participant_id) + dpath;
             data(end).raw_file = string(fnames{any(string(fnames.FIELD_NAME)==target_tasks,2),"FILE_NAME"})';
             data(end).file = cellstr(remedied_path + string(fnames{any(string(fnames.FIELD_NAME)==target_tasks,2),"FILE_NAME"})');
@@ -61,7 +57,6 @@ data(1) = [];
 
 %% Remedy the files
 % This step is simialr to the import and remedy sections of step1.
-
 for i = 1:length(data)
     EEG = [];
     eeg_files = data(i).raw_file;
@@ -69,6 +64,7 @@ for i = 1:length(data)
         setname = split(f, ".mat");
         setname = setname(1);
         if contains(setname, "-"), setname = replace(setname, "-", "_"); end
+        p2l.rawEEG = datarepo + string(data(i).subject) + dpath;
         tempload = load(p2l.rawEEG + f);
         EEG.(setname) = tempload.EEG;
     end
