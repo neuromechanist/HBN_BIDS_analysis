9e088662d765494b79e663b9000df0aca4b97bd4
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-02-14 10:21:00 -0800

    added the ability to run on all subj within a release

--- funcs/shell/run_preprcss.sh
+++ funcs/shell/run_preprcss.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for i in 'NDARGC170UK2'
-do
+for i in $(cat subjs_to_analyze.txt); do
 # echo $i  # sanity check
 sbatch -J $i --export=ALL,i=$i /home/sshirazi/_git/HBN_BIDS_analysis/funcs/slurm/preprcss_to-step2.slurm
 done
