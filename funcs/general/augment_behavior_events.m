dad2906fd5c139d3f96b93b8573593bfb2518186
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-09-27 14:39:56 -0700

    added the surround suppression event augmentation

--- funcs/general/augment_behavior_events.m
+++ funcs/general/augment_behavior_events.m
@@ -22,7 +22,7 @@ file = beh_path + filesep + subject + "_" + filename;
 beh_event = load(file);
 
 %% main loop
-if filename == "WISC_ProcSpeed.mat" % This is the symbol search task
+if filename == "WISC_ProcSpeed.mat"  % symbol search task
     % First check if we have the same number of events
     correct_resp = readtable("symbolSearch_correct_response.tsv","filetype","text"); correct_resp_vector = reshape(correct_resp.Variables,[],1);
     subj_resp = beh_event.par.activated_resp(:,:,1);
@@ -39,13 +39,13 @@ if filename == "WISC_ProcSpeed.mat" % This is the symbol search task
         EEG.event(eeg_event_idx(i)).correct_answer = correct_resp_vector(i);
     end
 
-elseif filename == "vis_learn.mat" % sequence learning task
+elseif filename == "vis_learn.mat"  % sequence learning task
     correct_resp = beh_event.par.sequence;
     subj_resp = beh_event.par.resp_click;
     % find the code for the last dot turning off
     type_toAdd_event = [32,33,34,35,50]; % the response should be added just before these events
     if beh_event.par.numrepet ~= size(beh_event.par.resp_click,1) % failsafe.
-        warning("subject's response mismatches number of reps in the squence learning task, skipping adding the events")
+        warning("EEG response count mismatches number of behavior reps in the sequence learning task, skipping adding the events")
         return
     end
     for i = 1:length(EEG.event)
@@ -55,4 +55,26 @@ elseif filename == "vis_learn.mat" % sequence learning task
         end
     end
 
+elseif filename == "SurroundSupp_Block1.mat" || filename == "SurroundSupp_Block2.mat"  % surround supression task
+    background_cont = beh_event.BGcon; % background or no background
+    background_cont_string = false(size(background_cont));
+    background_cont_string(background_cont==1) = true;
+    forground_cont = beh_event.CNTcon; % contrast  of the forground
+    stimulus_cond = beh_event.StimCond; % Stimulus condition
+
+    stim_on = find(string({EEG.event(:).type})=="stim_ON");
+    stim_off = find(string({EEG.event(:).type})=="stim_OFF");
+
+    if length(stim_off) == length(stim_on) && length(stim_off) == length(background_cont)
+        for i = stim_on
+            EEG.event(i).background = background_cont_string(i==stim_on);
+            EEG.event(i+1).background = background_cont_string(i==stim_on);
+            EEG.event(i).forground_contrast = forground_cont(i==stim_on);
+            EEG.event(i+1).forground_contrast = forground_cont(i==stim_on);
+            EEG.event(i).stimulus_cond = stimulus_cond(i==stim_on);
+            EEG.event(i+1).stimulus_cond = stimulus_cond(i==stim_on);            
+        end
+    else
+        warning("legnth of the stimulation on in EEG.event mismatches the behavior file, skipping adding the events.")
+    end
 end
