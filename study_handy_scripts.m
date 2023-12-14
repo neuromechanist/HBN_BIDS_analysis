31bc9e2e281b527befdbd34336dc316a1a50111d
Seyed Yahya Shirazi <shirazi@ieee.org>
2023-12-13 22:07:35 -0800

    add paths and waitbar for dippole update

--- study_handy_scripts.m
+++ study_handy_scripts.m
@@ -3,7 +3,9 @@ out_path = study_path + "derivatives\eeglab_tetst\";
 
 [STUDY, ALLEEG] = pop_importbids(char(study_path), 'eventtype','value','bidsevent','on','bidschanloc','on',...
     'outputdir',char(out_path),'bidstask','ThePresent');
-
+%% paths
+addpath('eeglab_dev')
+addpath(genpath('HBN_BIDS_analysis'))
 %% change the event type
 for e = 1:length(EEG)
     EEG(e) = replace_event_type(EEG(e));
@@ -12,12 +14,12 @@ end
 %% can we expand in place?
 expand_table = "the_present_stimulus-LogLumRatio.tsv";
 for e = 1:length(EEG)
-    EEG(e) = expand_events(EEG(e), expand_table, ["shot_number", "LLR"],'shots');
+    EEG(e) = expand_events(EEG(e), expand_table, ["shot_number", "LLR"],'shots', 0, 1);
 end
 ALLEEG = EEG;
 
 %% update the components
-% init_paths("linux", "expanse", "HBN", 1, false);
+p2l = init_paths("linux", "expanse", "HBN", 1, false);
 fs = string(filesep)+string(filesep);
 mergedSetName = "videoEEG";
 subj_list = string({STUDY.datasetinfo.subject});
@@ -48,7 +50,7 @@ subj_list = string({STUDY.datasetinfo.subject});
 subjs = squeeze(split(subj_list,"-")); subjs = subjs(:,2);
 
 % The following for loop only works because there is one dataset per subj.
-f = waitbar(0,'Updating stduies w/ ICLABEL comps','Name','please be very patient');
+f = waitbar(0,'Updating studies w/ ICLABEL comps','Name','please be very patient');
 for i = 1:length(subjs')
     [STUDY, EEG] = std_editset(STUDY, EEG, 'commands', {{'index' i ...
         'comps' ICA_STRUCT.(s).incr_comps}});
@@ -57,4 +59,3 @@ for i = 1:length(subjs')
 end
 close(f);
 ALLEEG = EEG;
-
