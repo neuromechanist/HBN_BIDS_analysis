15cd9d2ed93ee9fd36691041c6ddeb50cd1ce659
Seyed Yahya Shirazi <shirazi@ieee.org>
2024-04-18 18:43:21 -0700

    Add lookup_dataset_info to sortout the dataset flags.

---
+++ funcs/general/lookup_dataset_info.m
@@ -0,0 +1,24 @@
+function idx = lookup_dataset_info(STUDY, col_name, pattern)
+%Lookup_dataset_info Checks for flags in the STUDY.datasetinfo
+% This function will look up of for the patterns present in the coulmns of
+% the the STUDY.datasetinfo. This is particularly useful when the STUDY is
+% created from a BIDS dataset. The output is the number under
+% STUDY.datasetinfo.index.
+% If there are multiple column names, there will be the same number of
+% cell elements in the output.
+% If there are multiple patterns, the reuslts wil be the union of indices
+% that mathces the pattern.
+%
+% (c) Seyed Yahya Shirazi, 04/2024 SCCN, INC, UCSD
+
+%% set the variables
+all_cols = string(fieldnames(STUDY.datasetinfo))';
+if ~all(contains(col_name, all_cols))
+    error("Column names are not available in STUDY.datasetinfo. Please check.")
+end
+
+for c = string(col_name)
+    col_data = string({STUDY.datasetinfo(:).(c)});
+    full_idx = [STUDY.datasetinfo(:).index];
+    idx{c == string(col_name)} = full_idx(contains(col_data, string(pattern)));
+end
